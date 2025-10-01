818f518d326a436f06d093f9d2997d57b7257477
Opilane TTHK <opilane@tthk.ee>
2025-10-01 13:20:37 +0300

    midagi ei teinud

--- TallinnaRakenduslikKolledz/Models/Course.cs
+++ TallinnaRakenduslikKolledz/Models/Course.cs
@@ -1,7 +1,10 @@
-﻿namespace TallinnaRakenduslikKolledz.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledz.Models
 {
     public class Course
     {
+        [Key]
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
